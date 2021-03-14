c05c1f2f8fc4357067839251cc39252aa09e825b
Simon Liang <no.lhr0909@gmail.com>
2021-03-14 23:52:13 +0800

    updated for fetching pdf links from annualreports.com

--- cheerio.js
+++ cheerio.js
@@ -4,20 +4,23 @@ request(
   {
     method: 'GET',
     // url: "https://www.techstars.com/accelerators/colliers-proptech",
-    url: 'https://www.nextgrid.ai/seed/',
+    // url: 'https://www.nextgrid.ai/seed/',
+    url: 'https://www.annualreports.com/Company/tesla-motors',
   },
   (err, res, body) => {
     if (err) return console.error(err)
     // Fetch webstite
     const $ = cheerio.load(body)
+    const links = $('a')
+    console.log(JSON.stringify(links.map((i, el) => $(el).attr('href')).get().filter(href => href.endsWith('pdf'))))
     // Remove <footer>*</footer> content
-    $('footer').remove()
-    const contentBuffer = $('*')
-    // Return the text
-    let content = contentBuffer.text()
-    // Remove all new lines from content
-    content = content.replace(/(\r\n|\n|\r)/gm, '')
-    // Log output
-    console.log(console.log(content))
+    // $('footer').remove()
+    // const contentBuffer = $('*')
+    // // Return the text
+    // let content = contentBuffer.text()
+    // // Remove all new lines from content
+    // content = content.replace(/(\r\n|\n|\r)/gm, '')
+    // // Log output
+    // console.log(console.log(content))
   }
 )
